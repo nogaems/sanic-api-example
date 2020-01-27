f7c36a835818cc8d1ee888af1940b47974d58ec8
nogaems <nogaems@users.noreply.github.com>
2020-01-27 04:55:48 +0300

    auth: change entry-point url to be more consistent

--- app.py
+++ app.py
@@ -9,13 +9,12 @@ from api import api
 
 from auth import authenticate, retrieve_user
 
-app = Sanic()
+app = Sanic(__file__)
 
 app.blueprint(api, version=1)
 
-initialize(app, authenticate=authenticate, retrieve_user=retrieve_user)
+initialize(app, authenticate=authenticate,
+           retrieve_user=retrieve_user, url_prefix='/v1/api/auth')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8888)
-
-
